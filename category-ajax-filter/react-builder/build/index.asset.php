30d48c9e63559d138bd256b9d0fe2cb95000bce0
Sean Murphy <sean@iamseanmurphy.com>
2026-09-07 15:54:52 +0000

    3.0.3

--- category-ajax-filter/react-builder/build/index.asset.php
+++ category-ajax-filter/react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '97acd990b6c27ceb7336');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'b0cddbad9d037f55cde7');
